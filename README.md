2c1bafd3d09e7ccad9030dfb254f584bded7a2c4
emreutkan <127414322+emreutkan@users.noreply.github.com>
2025-04-14 16:51:53 +0300

    quicklinks mock api, api call with ts, css

--- README.md
+++ README.md
@@ -3,15 +3,6 @@ Midterm Project for Software Engineering class SE-3355 Web Development
 
 
 ```aiignore
-[
-  { "title": "Sepette %20 İndirim" },
-  { "title": "Kargo Bedava Ürünler" },
-  { "title": "Haftanın Fırsatları" },
-  { "title": "Sadece Bugün Geçerli" },
-  { "title": "Elektronik Süper Fiyatlar" },
-  { "title": "Moda Özel İndirim" },
-  { "title": "3 Al 2 Öde" },
-  { "title": "Yıldızlı Ürünler" }
-]
+
 
 ```
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,41 @@
+
+const QUICK_LINKS_API = "https://run.mocky.io/v3/d3d75ffd-97a5-4ea5-9824-91c60faa72d8";
+
+// [
+//     { "topText": "Bu Fiyatlar Kaçmaz", "title": "Elektronik", "category": "Elektronik" },
+//     { "topText": "Fırsatlarla Dolu", "title": "Çeyiz ALIŞVERİŞİ", "category": "Alışverişe Başla" },
+//     { "topText": "Kredi Limitini Gör", "title": "%0'DAN BAŞLAYAN FAİZ", "category": "Hepsipay" },
+//     { "topText": "İndirimli Fiyatlar", "title": "DÖRT DÖRTLÜK FIRSATLAR", "category": "Alışverişe Başla" },
+//     { "topText": "Fırsatları Keşfet", "title": "ALIŞVERİŞİN TOP LİSTESİ", "category": "Sevilen Ürünler" },
+//     { "topText": "Her Pazar Fırsat Var", "title": "NET İNDİRİM PAZARI", "category": "Sakın Kaçırma" },
+//     { "topText": "Aradığın Fırsatlar", "title": "Ev Elektroniği", "category": "Elektronik" },
+//     { "topText": "Büyük İndirim", "title": "OYUNCAK GÜNLERİ", "category": "Sevilen Oyuncaklar" }
+// ]
+
 type QuickLink = {
+    topText: string;
     title: string;
+    category: string;
 };
 
-const QUICK_LINKS_API = "https://run.mocky.io/v3/5b33545a-cd3d-4a1f-869c-343a7f7b8ca1";
 
 const fetchQuickLinks = async () => {
     try {
         const res = await fetch(QUICK_LINKS_API);
         const data: QuickLink[] = await res.json();
-
         const container = document.getElementById("quick-links");
         if (!container) return;
 
         data.forEach((item) => {
             const card = document.createElement("div");
             card.className = "quick-link-card";
-            card.textContent = item.title;
+
+            card.innerHTML = `
+        <div class="card-top">${item.topText}</div>
+        <div class="card-title">${item.title}</div>
+        <div class="card-category">${item.category}</div>
+      `;
+
             container.appendChild(card);
         });
     } catch (err) {
